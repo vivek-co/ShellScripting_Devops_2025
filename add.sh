a0e7d881e9907e5dc7e4c00db0f1eaa2ac172723
vivek-co <vivekco1998@gmail.com>
2025-11-11 13:24:44 +0000

    added files to another folder

--- add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-var="good"
-var1="Morning"
-num=10
-echo "the value of variable $var, $var1"
-echo "the value of $num"
-
--- deletelastbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls -lrt | awk -F " " 'NR>1 {print $NF}' > outputfile
-total=`cat outputfile | wc -l`
-echo "enter the number of values to retain"
-read count
-count=`expr $total - $count`
-head -$count outputfile
-
